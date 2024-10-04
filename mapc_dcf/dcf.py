d683d573c6fbe3052b10ab90567c596df7d39b8a
Wojciech Ciezobka <wciezobka@student.agh.edu.pl>
2024-10-04 16:47:38 +0200

    Better description in logs

--- mapc_dcf/dcf.py
+++ mapc_dcf/dcf.py
@@ -36,6 +36,12 @@ class DCF():
         self.total_collisions = 0
     
 
+    def timestamp(self):
+        t = f"{10**6 * self.des_env.now:.9f} us"
+        leading_zeros = max(6 - len(t.split(".")[0]), 0)
+        return "0" * leading_zeros + t
+    
+
     def start_operation(self, run_number: int):
         self.run_number = run_number
         self.des_env.process(self._run())
@@ -49,38 +55,38 @@ class DCF():
         
         idle = self.channel.is_idle_for(self.des_env.now, DIFS, frame.src, frame.tx_power)
         while not idle:
-            logging.info(f"AP{self.ap}:t{self.des_env.now:.9f}\t Channel busy, waiting for a SLOT_TIME = 9 us")
+            logging.info(f"AP{self.ap}:{self.timestamp()}\t DIFS: waiting for a SLOT_TIME = 9 us")
             yield self.des_env.timeout(SLOT_TIME)
             idle = self.channel.is_idle_for(self.des_env.now, DIFS, frame.src, frame.tx_power)
-        logging.info(f"AP{self.ap}:t{self.des_env.now:.9f}\t Channel idle for DIFS = 34 us")
+        logging.info(f"AP{self.ap}:{self.timestamp()}\t DIFS: Channel idle for DIFS = 34 us")
     
 
     def _freeze_backoff(self, frame: WiFiFrame, time_to_backoff: int):
 
-        logging.info(f"AP{self.ap}:t{self.des_env.now:.9f}\t Channel busy, freezing backoff at TTB = {time_to_backoff}")
+        logging.info(f"AP{self.ap}:{self.timestamp()}\t Channel busy, freezing backoff at TTB = {time_to_backoff}")
         yield self.des_env.process(self._wait_for_difs(frame))
-        logging.info(f"AP{self.ap}:t{self.des_env.now:.9f}\t Channel idle, reactivating backoff at TTB = {time_to_backoff}")
+        logging.info(f"AP{self.ap}:{self.timestamp()}\t Channel idle, reactivating backoff at TTB = {time_to_backoff}")
         # and reactivated after the channel is sensed idle again for a guard period.
 
 
     def _try_sending(self, frame: WiFiFrame, retry_count: int):
 
-        logging.info(f"AP{self.ap}:t{self.des_env.now:.9f}\t Attempting to send frame. Retry count: {retry_count}")
+        logging.info(f"AP{self.ap}:{self.timestamp()}\t Attempting to send frame. Retry count: {retry_count}")
         
         # If the retry limit is reached, the frame is dropped
         if retry_count > self.retry_limit:
-            logging.info(f"AP{self.ap}:t{self.des_env.now:.9f}\t Retry limit reached, dropping frame")
+            logging.info(f"AP{self.ap}:{self.timestamp()}\t Retry limit reached, dropping frame")
             return
 
         # Initialize a random backoff interval
         key_backoff, self.key = jax.random.split(self.key)
         initialized_backoff = jax.random.randint(key_backoff, shape=(1,), minval=0, maxval=self.cw).item()
-        logging.info(f"AP{self.ap}:t{self.des_env.now:.9f}\t TTB initialized as {initialized_backoff} from [0, {self.cw}) interval")
+        logging.info(f"AP{self.ap}:{self.timestamp()}\t TTB initialized as {initialized_backoff} from [0, {self.cw}) interval")
 
         # The bakoff countdown with the freeze-and-reactivation mechanism
         time_to_backoff = initialized_backoff
         while time_to_backoff > 0:
-            logging.info(f"AP{self.ap}:t{self.des_env.now:.9f}\t TTB = {time_to_backoff}")
+            logging.info(f"AP{self.ap}:{self.timestamp()}\t TTB = {time_to_backoff}")
 
             # The backoff time counter is decremented as long as the channel is sensed idle.
             if self.channel.is_idle(self.des_env.now, frame.src, frame.tx_power):
@@ -98,7 +104,7 @@ class DCF():
         #     # and reactivated after the channel is sensed idle again for a guard period.
         
         # The frame is sent to the channel
-        logging.info(f"AP{self.ap}:t{self.des_env.now:.9f}\t TTB reached zero (TTB = {time_to_backoff}) and the channel is idle. Sending frame to {frame.dst}")
+        logging.info(f"AP{self.ap}:{self.timestamp()}\t TTB reached zero (TTB = {time_to_backoff}) and the channel is idle. Sending frame to {frame.dst}")
         self.channel.send_frame(frame, self.des_env.now, retry_count)
         yield self.des_env.timeout(frame.duration + SIFS) # The SIFS is the lower bound of the ACK timeout
 
@@ -111,14 +117,14 @@ class DCF():
 
         # If the packet transmission is unsuccessful, the size of the contention window is doubled
         if collision:
-            logging.info(f"AP{self.ap}:t{self.des_env.now:.9f}\t Collision, increasing CW to {self.cw}")
+            logging.info(f"AP{self.ap}:{self.timestamp()}\t Collision, increasing CW to {self.cw}")
             self.total_collisions += 1
             self.cw = min(2*self.cw, 2**CW_EXP_MAX)
             yield self.des_env.process(self._try_sending(frame, retry_count + 1))
         
         # and reset, if successful
         else:
-            logging.info(f"AP{self.ap}:t{self.des_env.now:.9f}\t TX successfull, resetting CW to {self.cw}")
+            logging.info(f"AP{self.ap}:{self.timestamp()}\t TX successfull, resetting CW to {self.cw}")
             self.cw = 2**CW_EXP_MIN
 
 
@@ -128,7 +134,7 @@ class DCF():
         the documentation `\\docs\\diagrams\\DCF_simple.pdf`.
         """
 
-        logging.info(f"AP{self.ap}:t{self.des_env.now:.9f}\t DCF running")
+        logging.info(f"AP{self.ap}:{self.timestamp()}\t DCF running")
 
         # Network is assumed to be saturated, there is always a frame to send
         while True:
